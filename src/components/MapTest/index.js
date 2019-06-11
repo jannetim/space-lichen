2c24519b6948e15a1ac38fdeb6ca8ed5aeed59b3
Janne Timonen <janne.timonen@perfektio.fi>
2019-06-12 00:07:10 +0300

    latlng click example

--- src/components/MapTest/index.js
+++ src/components/MapTest/index.js
@@ -3,9 +3,10 @@ import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 
 const position = [51.505, -0.09]
 const TestMap = ({ height, width }) => {
+  const onMapClick = (e) => console.log("latlng", e.latlng)
   return (
     <div>
-      <Map center={position} style={{ height, width }} zoom={13}>
+      <Map center={position} style={{ height, width }} zoom={13} onClick={(e) => onMapClick(e)}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
