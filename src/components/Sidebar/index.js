3d88dfb3b41affb3e69a13c57db5eebe991846df
Nico Roos <nico.roos@perfektio.fi>
2019-06-12 17:37:23 +0300

    First combined chart.

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { chart1, chart2, chart3 } from './Charts'
 
 const Sidebar = ({
@@ -8,11 +8,15 @@ const Sidebar = ({
   data2,
   data3,
 }) => {
+  const [slice, setSlice] = useState([0, 30])
   return (
-    <div style={{ height, width, padding: 20, boxSizing: 'border-box' }}>
-      { chart1() }
-      { chart2() }
-      { chart3() }
+    <div
+      style={{ height, width, padding: 20, boxSizing: 'border-box' }}
+      onClick={() => {
+        setSlice([slice[0] + 30, slice[1] + 30])}
+      }
+    >
+      { chart3(data1.slice(...slice)) }
     </div>
   );
 };
