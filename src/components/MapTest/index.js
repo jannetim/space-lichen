2d7dc29ded62345db9f74df55bde01b844cfbc6c
Nico Roos <nico.roos@perfektio.fi>
2019-06-13 03:08:26 +0300

    Donotremovearea.

--- src/components/MapTest/index.js
+++ src/components/MapTest/index.js
@@ -7,39 +7,35 @@ const position = [67.413, 26.5950]
 
 const TestMap = ({ height, width, pickArea, forecast }) => {
   const onMapClick = (e) => console.log("latlng", e.latlng)
-  //sodCosmic()
-  //fetch()
   return (
-      <Map center={position} style={{ height, width }} zoom={13} onClick={(e) => onMapClick(e)}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-        />
-        <Marker position={position}>
-          <Popup 
-            onOpen={() => pickArea('Sodankylä')}
-            onClose={() => pickArea('')}  
-          >Sodankylä<br />Weather forecast 5d/3h
-            {popUpChart2(forecast)}
-          </Popup>
-        </Marker>
-        <Marker position={[ 66.50108687066692, 25.71857243773935]}>
-          <Popup
-            onOpen={() => pickArea('Rovaniemi')}
-            onClose={() => pickArea('')}   
-          >Rovaniemi<br />Weather forecast 5d/3h
-            {popUpChart2(forecast)}
-          </Popup>
-        </Marker>
-        <Marker position={[65.01113023989858, 25.48192739884203]}>
-          <Popup
-            onOpen={() => pickArea('Oulu')}
-            onClose={() => pickArea('')}   
-          >Oulu<br />Weather forecast 5d/3h
-            {popUpChart2(forecast)}
-          </Popup>
-        </Marker>
-      </Map>
-)}
+    <Map center={position} style={{ height, width }} zoom={13} onClick={(e) => onMapClick(e)}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+      />
+      <Marker position={position}>
+        <Popup
+          onOpen={() => pickArea('Sodankylä')}
+        >Sodankylä<br />Weather forecast 5d/3h
+          {popUpChart2(forecast)}
+        </Popup>
+      </Marker>
+      <Marker position={[ 66.50108687066692, 25.71857243773935]}>
+        <Popup
+          onOpen={() => pickArea('Rovaniemi')}
+        >Rovaniemi<br />Weather forecast 5d/3h
+          {popUpChart2(forecast)}
+        </Popup>
+      </Marker>
+      <Marker position={[65.01113023989858, 25.48192739884203]}>
+        <Popup
+          onOpen={() => pickArea('Oulu')}
+        >Oulu<br />Weather forecast 5d/3h
+          {popUpChart2(forecast)}
+        </Popup>
+      </Marker>
+    </Map>
+  )
+}
 
 export default TestMap
